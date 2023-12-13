084d242bc83818ecc73d4bf5591d087dcca67b89
Gaurav Srivastava <fiftybugsofgrey@gmail.com>
2023-12-13 20:26:43 +0530

    done hard(promises) problems

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,18 +5,33 @@
  */
 
 function wait1(t) {
-
+  return new Promise(function(resolve) {
+    setTimeout(resolve, t * 1000)
+  })
 }
 
 function wait2(t) {
-
+  return new Promise(function(resolve) {
+    setTimeout(resolve, t * 1000)
+  })
 }
 
 function wait3(t) {
-
+  return new Promise(function(resolve) {
+    setTimeout(resolve, t * 1000)
+  })
 }
 
 function calculateTime(t1, t2, t3) {
+  let start = new Date().getTime();
+
+  return Promise.all([
+    wait1(t1),
+    wait2(t2),
+    wait3(t3)
+  ]).then(function(responses) {
+    return new Date().getTime() - start;
+  })
 
 }
 
